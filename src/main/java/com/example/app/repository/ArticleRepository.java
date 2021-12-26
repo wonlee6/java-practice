2a49314671301678434e1a153ba676d800d33c73
wonlee6 <wonlee6@gmail.com>
2021-12-27 03:46:20 +0900

    H2 sql 디비 입력

--- src/main/java/com/example/app/repository/ArticleRepository.java
+++ src/main/java/com/example/app/repository/ArticleRepository.java
@@ -4,6 +4,4 @@ import com.example.app.entity.Article;
 import org.springframework.data.repository.CrudRepository;
 
 public interface ArticleRepository extends CrudRepository<Article, Long> {
-
-
 }
